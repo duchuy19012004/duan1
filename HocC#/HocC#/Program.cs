19c706e9af898cb188a254a1ca9da97c7f1d3522
duchuy19012004 <zzcz4991@gmail.com>
2025-05-12 20:26:25 +0700

    update lan 3

---
+++ HocC#/HocC#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HocC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap noi dung: ");
+            string text = Console.ReadLine();
+            Console.WriteLine($"Noi dung la: {text}");
+        }
+    }
+}
